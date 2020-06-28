6876b127a195dc21d126910c8d7bc0daf2df4e1f
ganzes <charczuk.arkadiusz@gmail.com>
2020-06-28 23:40:43 +0200

    heroku 9

--- src/main/java/com/kodilla/eprojectkfrontend/views/MainView.java
+++ src/main/java/com/kodilla/eprojectkfrontend/views/MainView.java
@@ -9,7 +9,7 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-@Route("mainView")
+@Route("")
 public class MainView extends VerticalLayout {
 
     private Button goToMotiveView = new Button("Go to Motives!");
